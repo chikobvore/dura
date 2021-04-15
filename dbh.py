95f0aaa5a783e51b04862a356ef3b1b6702b4a6f
Chikobvore <nchikobvor@gmail.com>
2021-04-15 10:01:27 -0700

    changed database connection

--- dbh.py
+++ dbh.py
@@ -2,7 +2,8 @@
 #this script handles the database connections
 import pymongo
 
-client = pymongo.MongoClient('localhost', 27017)
-db = client['dura']
-# client = pymongo.MongoClient("mongodb+srv://ladsroot:ladsroot@lads.f97uh.mongodb.net/tau?retryWrites=true&w=majority")
-# db = client.tau
+# client = pymongo.MongoClient('localhost', 27017)
+# db = client['dura']
+
+client = pymongo.MongoClient("mongodb+srv://dura:ei3TOR5FpldWCVZV@cluster0.ye35n.mongodb.net/dura?retryWrites=true&w=majority")
+db = client.tau
